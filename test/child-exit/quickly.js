59cef013c23cceb0fa61c5e45a4c47dff7e841fa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-05-02 00:19:50 -0400

    added example with two services in the single quickly file

--- test/child-exit/quickly.js
+++ test/child-exit/quickly.js
@@ -1,3 +1,9 @@
+/*
+  The single child service here exits after
+  printing a message to the console.
+  The system should at least print the notification
+  that the child process has exitted.
+*/
 module.exports = {
   test: {
     exec: 'echo',
---
+++ test/two-choices/quickly.js
@@ -0,0 +1,14 @@
+/*
+  The system should ask which service to run
+  from this file.
+*/
+module.exports = {
+  test1: {
+    exec: 'echo',
+    args: 'running test 1'
+  },
+  test2: {
+    exec: 'echo',
+    args: 'running test 2'
+  }
+};
